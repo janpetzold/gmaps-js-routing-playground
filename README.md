c05d86fe55e24555f23fdbffa228ff069d0d4c75
Jan Petzold <janpetzold@outlook.com>
2024-04-23 11:25:35 +0300

    Reverted to top-down view for performance, various fixes in routing, updated README with measurements

--- README.md
+++ README.md
@@ -10,9 +10,21 @@ Sample route:
 
 URL: https://maps.app.goo.gl/5t2vbyiqKXTZQoM69
 
-For decoding & polyline genration see `python`.
+For decoding & polyline generation see `python`.
 
 For web frontend see `web`. Run this locally via
 
     cd web
-    http-server -p 8080
+    busybox httpd -f -p 8080 &
+
+# Results
+
+When measuring frames per second the Zero 2 achieved TODO fps (benchmark here TODO).
+
+Measuring power consumption resulted in the following:
+
+Raspberry Pi Zero 2 idle: 0.145A
+External display w. 800*480 resolution: 0.29A
+Raspberry Pi Zero 2 with routing benchmark running: 0.26A
+
+So in total this means when routing we end up with power consumption of ~0.55A. Using a $15 2500mAh battery would lead to a max runtime of approx. 5 hours.
--- python/decode-route.py
+++ python/decode-route.py
@@ -1,5 +1,6 @@
 import re
 import base64
+import json
 import requests
 from dotenv import load_dotenv
 import googlemaps
@@ -7,6 +8,19 @@ import os
 from datetime import datetime
 
 load_dotenv()
+
+def extract_route_details(directions_result):
+    route_details = []
+    # Assuming directions_result is a list and we take the first route
+    for leg in directions_result[0]["legs"]:
+        for step in leg["steps"]:
+            detail = {
+                "start_location": step["start_location"],  # Get from step
+                "end_location": step["end_location"],      # Get from step
+                "html_instructions": step["html_instructions"]  # Get from step
+            }
+            route_details.append(detail)
+    return route_details
 
 # Link from Google Maps for given route with four waypoints
 gmaps_url = 'https://maps.app.goo.gl/5t2vbyiqKXTZQoM69'
@@ -31,18 +45,30 @@ gmaps = googlemaps.Client(os.getenv("GMAPS_API_KEY"))
 now = datetime.now()
 directions_result = gmaps.directions(str(gps_coordinates_reversed[0]).replace("(", "").replace(")", ""),
                                      str(gps_coordinates_reversed[3]).replace("(", "").replace(")", ""),
-                                     mode="walking",
+                                     mode="driving",
                                      departure_time=now,
+                                     units="metric",
                                      waypoints=[
                                         str(gps_coordinates_reversed[1]).replace("(", "").replace(")", ""),
                                         str(gps_coordinates_reversed[2]).replace("(", "").replace(")", "")
                                      ])
 
-print("Polyline of route is as follows. Verify it e.g. via https://valhalla.github.io/demos/polyline/.")
-print(directions_result[0]["overview_polyline"]["points"])
+#print("Polyline of route is as follows. Verify it e.g. via https://valhalla.github.io/demos/polyline/.")
+#print(directions_result[0]["overview_polyline"]["points"])
 
-print("Base64 encoding")
+route_details = extract_route_details(directions_result)
+#print(route_details)
+
+json_string = json.dumps(route_details)
+base64_bytes_route = base64.b64encode(json_string.encode('utf-8'))
+
+print("+++++")
+print("Route details as Base64:")
+print(base64_bytes_route)
+
 message_bytes = directions_result[0]["overview_polyline"]["points"].encode('ascii')
-base64_bytes = base64.b64encode(message_bytes)
+base64_bytes_polyline = base64.b64encode(message_bytes)
 
-print(base64_bytes)
+print("+++++")
+print("Polyline as Base64:")
+print(base64_bytes_polyline)
